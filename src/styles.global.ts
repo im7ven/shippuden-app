ca79924dc0d7653582644c1940297209eee38d9d
Justin <alexanderrr416@gmail.com>
2023-10-25 23:14:39 -0400

    Adjust background.

--- src/styles.global.ts
+++ src/styles.global.ts
@@ -9,6 +9,7 @@ html {
 body {
     margin: 0;
     box-sizing: border-box;
+    background: rgb(56,56,56);
     background: radial-gradient(circle, rgba(55,55,55,1) 14%, rgba(0,0,0,1) 100%);
     min-height: 100vh;
 
